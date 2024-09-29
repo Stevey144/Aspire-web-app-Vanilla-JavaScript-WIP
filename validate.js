679af9ee2400336bf83f9fcaf36294a3678d26d0
stevey144 <stephenogechi455@gmail.com>
2024-09-29 16:52:03 +0100

    updated

--- validate.js
+++ validate.js
@@ -29,8 +29,8 @@ function SingUp(){
 
         localStorage.setItem('email', user_email);
         localStorage.setItem('password',user_password);
+        window.location.href = 'appointment.html';
         alert("Success, please proceed to sign in Page to book Appointment");
-        window.location.replace('appointment.html');
 }
 
 
