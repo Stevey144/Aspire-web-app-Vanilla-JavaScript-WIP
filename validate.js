e8d1a100e7d68f744549bc5d284425cde376fa08
stevey144 <stephenogechi455@gmail.com>
2024-09-29 19:19:45 +0100

    removed commented file

--- validate.js
+++ validate.js
@@ -82,18 +82,3 @@ $(document).ready(function() {
 
 });
 
-
-
-// function bookAppointment(){
-//          var date_of_appointment;
-//          var reason_for_appointment;
-//          date_of_appointment = document.getElementById('date').value;
-//          reason_for_appointment = document.getElementById('reason_for_booking').value;
-
-//          if(date_of_appointment && reason_for_appointment){
-//             alert("you have been successfully scheduled for an appointment for the given date " + date_of_appointment + " please plan to attend.");
-//          }
-//        else{
-//                 alert("unable to schedule you for an appointment");
-//          }
-// }
